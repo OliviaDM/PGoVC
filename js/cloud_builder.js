22b1a72b96ceb7aec93f5e7dc7db8e9eb8c982b2
Olivia De Meyere <oliviademeyere@gmail.com>
2020-02-24 15:24:20 +0100

    links work back to previous version

--- js/cloud_builder.js
+++ js/cloud_builder.js
@@ -1,20 +1,16 @@
 
-
 function cloud() {
-    // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 
-    // let uniforms = {
-    //     colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
-    //     colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)}
-    // }
-    // const material = new THREE.ShaderMaterial({
-    //     uniforms: uniforms,
-    //     fragmentShader: fs,
-    //     vertexShader: vs,
-    // });
+    let uniforms = {
+        colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
+        colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)}
+    }
+    const material = new THREE.ShaderMaterial({
+        uniforms: uniforms,
+        fragmentShader: frag_shade,
+        vertexShader: vert_shade,
+    });
 
-    // return new THREE.Mesh( geometry, material );
-
-    console.log(2);
+    return new THREE.Mesh( geometry, material );
 }
-
--- js/main.js
+++ js/main.js
@@ -1,20 +1,71 @@
 // import * as THREE from '/three.js-master/build/three.js';
 
-function main () {
-    cloud();
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+function main (fs, vs) {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-    var renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
+    const canvas = document.querySelector("canvas");
 
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
+    const cube = cloud(fs, vs);
     scene.add( cube );
 
     camera.position.z = 5;
+
+
+
+    let mouseDown = false,
+        mouseX = 0,
+        mouseY = 0;
+
+    function onMouseMove(evt) {
+        if (!mouseDown) {
+            return;
+        }
+
+        evt.preventDefault();
+        let deltaX = evt.clientX - mouseX,
+            deltaY = evt.clientY - mouseY;
+        mouseX = evt.clientX;
+        mouseY = evt.clientY;
+        rotateScene(deltaX, deltaY);
+    }
+
+    function onMouseDown(evt) {
+        evt.preventDefault();
+
+        mouseDown = true;
+        mouseX = evt.clientX;
+        mouseY = evt.clientY;
+    }
+
+    function onMouseUp(evt) {
+        evt.preventDefault();
+        mouseDown = false;
+    }
+
+    canvas.addEventListener('mousemove', function (e) {
+        onMouseMove(e);
+    }, false);
+    canvas.addEventListener('mousedown', function (e) {
+        onMouseDown(e);
+    }, false);
+    canvas.addEventListener('mouseup', function (e) {
+        onMouseUp(e);
+    }, false);
+    
+
+    function rotateScene(deltaX, deltaY) {
+        cube.rotation.y += deltaX / 100;
+        cube.rotation.x += deltaY / 100;
+    }
+
+
+
 
     function animate() {
         requestAnimationFrame( animate );
