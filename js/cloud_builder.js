9d264f2b374223719064fe36287def41b5a61e36
Olivia De Meyere <oliviademeyere@gmail.com>
2020-02-24 15:10:20 +0100

    links probably alright + shaders copied

---
+++ js/cloud_builder.js
@@ -0,0 +1,20 @@
+
+
+function cloud() {
+    // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+
+    // let uniforms = {
+    //     colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
+    //     colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)}
+    // }
+    // const material = new THREE.ShaderMaterial({
+    //     uniforms: uniforms,
+    //     fragmentShader: fs,
+    //     vertexShader: vs,
+    // });
+
+    // return new THREE.Mesh( geometry, material );
+
+    console.log(2);
+}
+
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // import * as THREE from '/three.js-master/build/three.js';
 
 function main () {
+    cloud();
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
