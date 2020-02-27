c141c25ae6923ea34056b8d9b7b681a77d69ea64
Olivia De Meyere <oliviademeyere@gmail.com>
2020-02-27 15:50:36 +0100

    z coordinate corrected, camera removed from step_dir initialisation

--- js/shaders.js
+++ js/shaders.js
@@ -41,34 +41,78 @@ varying vec4 new_x;
 varying vec4 new_y;
 varying vec4 new_z;
 
+bool is_inside_vox(in vec3 pos) {
+    float scalar_comp_x = dot(pos - new_vox_pos.xyz, (new_x.xyz - new_vox_pos.xyz)/length(new_x.xyz - new_vox_pos.xyz))/length(new_x.xyz - new_vox_pos.xyz);
+    float scalar_comp_y = dot(pos - new_vox_pos.xyz, (new_y.xyz - new_vox_pos.xyz)/length(new_y.xyz - new_vox_pos.xyz))/length(new_y.xyz - new_vox_pos.xyz);
+    float scalar_comp_z = dot(pos - new_vox_pos.xyz, (new_z.xyz - new_vox_pos.xyz)/length(new_z.xyz - new_vox_pos.xyz))/length(new_z.xyz - new_vox_pos.xyz);
+    return ((scalar_comp_x > 0.0) && (scalar_comp_x < 1.0) && (scalar_comp_y > 0.0) && (scalar_comp_y < 1.0) && (scalar_comp_z > 0.0) && (scalar_comp_z < 1.0));
+}
+
+vec3 scalar_val(in vec3 pos) {
+    float scalar_comp_x = dot(pos - new_vox_pos.xyz, (new_x.xyz - new_vox_pos.xyz)/length(new_x.xyz - new_vox_pos.xyz))/length(new_x.xyz - new_vox_pos.xyz);
+    float scalar_comp_y = dot(pos - new_vox_pos.xyz, (new_y.xyz - new_vox_pos.xyz)/length(new_y.xyz - new_vox_pos.xyz))/length(new_y.xyz - new_vox_pos.xyz);
+    float scalar_comp_z = dot(pos - new_vox_pos.xyz, (new_z.xyz - new_vox_pos.xyz)/length(new_z.xyz - new_vox_pos.xyz))/length(new_z.xyz - new_vox_pos.xyz);
+    return vec3(scalar_comp_x, scalar_comp_y,scalar_comp_z);
+}
+
+float sample_value(in vec3 pos) {
+    return 1.0;
+}
+
 void main() {
     float step_size = 0.01;
-    vec3 cur_pos = vUv.xyz + new_vox_pos.xyz;
+    vec3 cur_pos = vUv.xyz;
     vec3 camera_pos = vec3(0.0,0.0,5.0);
-    vec3 step_dir = normalize(cur_pos - camera_pos);
-    bool inside = true;
+    vec3 step_dir = normalize(cur_pos);
+    int max_steps = int(box_dim/step_size);
+    float count = 0.0;
+    
+    float cummulative_value = 0.0;
 
-    float self_product = dot(new_y.xyz - new_vox_pos.xyz, new_y.xyz - new_vox_pos.xyz);
-
-    float dot_x = dot(vUv.xyz - new_vox_pos.xyz, new_x.xyz - new_vox_pos.xyz);
-    float dot_y = dot(vUv.xyz - new_vox_pos.xyz, new_y.xyz - new_vox_pos.xyz)/dot(new_y.xyz - new_vox_pos.xyz, new_y.xyz - new_vox_pos.xyz);
-    float dot_z = dot(vUv.xyz - new_vox_pos.xyz, new_z.xyz - new_vox_pos.xyz)/dot(new_z.xyz - new_vox_pos.xyz, new_z.xyz - new_vox_pos.xyz);
-
-    if (length(vUv.xyz - new_vox_pos.xyz) < 0.01) {
-
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-    } else if (length(vUv.xyz - new_x.xyz) < 0.01) {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    } else if (length(vUv.xyz - new_y.xyz) < 0.01) {
-        gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
-    } else if (length(vUv.xyz - new_z.xyz) < 0.01) {
-        gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
-    } else {
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 0.1);
+    for (int i = 0; i < 500; i += 1) {
+        cummulative_value += 0.1;
+        cur_pos = cur_pos + step_dir*step_size;
+        // if (!is_inside_vox(cur_pos)) {
+        //     break;
+        // }
+        count += 1.0;
     }
-}   
-`;
+    
+    // if (length(vUv.xyz - new_vox_pos.xyz) < 0.01) {
+    //     gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    // } else if (length(vUv.xyz - new_x.xyz) < 0.01) {
+    //     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    // } else if (length(vUv.xyz - new_y.xyz) < 0.01) {
+    //     gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    // } else if (length(vUv.xyz - new_z.xyz) < 0.01) {
+    //     gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+    // } else {
+    //     if (scalar_val(cur_pos).x > 1.0) {
+    //         gl_FragColor = vec4(1.0, 0.0, 0.0, 0.2);
+    //     } else if (scalar_val(cur_pos).y > 1.0) {
+    //         gl_FragColor = vec4(0.0, 1.0, 0.0, 0.2);
+    //     } else if (scalar_val(cur_pos).z > 1.0) {
+    //         gl_FragColor = vec4(0.0, 0.0, 1.0, 0.2);
+    //     } else if (scalar_val(cur_pos).x < 0.0) {
+    //         gl_FragColor = vec4(1.0, 0.0, 1.0, 0.2);
+    //     } else if (scalar_val(cur_pos).y < 0.0) {
+    //         gl_FragColor = vec4(1.0, 1.0, 0.0, 0.2);
+    //     } else if (scalar_val(cur_pos).z < 0.0) {
+    //         gl_FragColor = vec4(0.0, 1.0, 1.0, 0.2);
+    //     } else {
+    //         gl_FragColor = vec4(scalar_val(cur_pos), 0.05);
+    //     }
+    // }
+
+    gl_FragColor = vec4(scalar_val(cur_pos), 1.0);
+    
+}`;
 
 // general sampling function: while loop which cumulates the final opacity
 // general colour function for a single sample point (will eventually call mulitple functions)
 // test the inclusion of a point inside the cube
+
+// for (int j = 0; j < 10; j+= 1) {
+//     cummulative_value += 0.1;
+//     cur_pos = cur_pos + step_dir*step_size;
+// }
